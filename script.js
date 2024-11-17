05a1e2dc0f83286e3b3ca242549f418ece839713
XabibullayevAbror <abrorboiiixabibullayev@gmail.com>
2024-11-17 17:20:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let text = document.querySelector('h1')
+let text2 = document.querySelector('.box')
+
+let a = document .getElementsByClassName('box')
+let b = document.getElementById('box')
+
+text.innerHTML = 'salom'
